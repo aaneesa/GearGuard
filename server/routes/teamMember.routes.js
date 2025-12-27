27363e7f627844981685ad398eca63539f90f9cc
aaneesa <anweshaa002@gmail.com>
2025-12-27 16:18:07 +0530

    request api

--- server/routes/teamMember.routes.js
+++ server/routes/teamMember.routes.js
@@ -5,10 +5,10 @@ const { authenticate } = require("../middlewares/authMiddleware");
 const { authorize } = require("../middlewares/roleMiddleware");
 
 // Add member to team (ADMIN or MANAGER)
-router.post("/", authenticate, authorize(["ADMIN", "MANAGER"]), controller.add);
+router.post("/", authenticate, authorize("TECHNICIAN"), controller.add);
 
 // Remove member from team (ADMIN or MANAGER)
-router.delete("/:id", authenticate, authorize(["ADMIN", "MANAGER"]), controller.remove);
+router.delete("/:id", authenticate, authorize("ADMIN", "MANAGER"), controller.remove);
 
 // Get members of a specific team
 router.get("/team/:teamId", authenticate, controller.getByTeam);
